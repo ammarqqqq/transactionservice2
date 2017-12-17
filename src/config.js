ccdfe2f8e1ab162bcedbea47bf2b7ecadd21147a
Ammar Iqbal <ammar.iqbal@gmail.com>
2017-12-17 19:47:38 +0100

    -

--- src/config.js
+++ src/config.js
@@ -5,14 +5,14 @@ module.exports = function(){
               'secret': '04050405',
               'database': 'mongodb://localhost:27013/transaction_integration_test',
               'logsDirectory': 'logs',
-              'serviceName': 'transactionservice'
+              'serviceName': 'microservices_transactionservice'
             };
         default:
             return {
               'secret': '04050405',
               'database': 'mongodb://transactionmongo:27013/transaction',
               'logsDirectory': 'logs',
-              'serviceName': 'transactionservice'
+              'serviceName': 'microservices_transactionservice'
             };
     }
 };
